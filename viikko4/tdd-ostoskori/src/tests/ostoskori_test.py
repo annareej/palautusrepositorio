886df0b67e19b4e93a6c453f9cd31bf541af1ac2
Anna-Reetta Julkunen <ar.julkunen@gmail.com>
2023-11-27 22:22:05 +0200

    viikko4 tdd 5 kahden tuottteen hinta testi

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -22,13 +22,21 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(self.kori.hinta(), 3)
 
-    def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         mehu = Tuote("Mehu", 5)
         self.kori.lisaa_tuote(mehu)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korin_hinta_sama_kuin_tuotteiden_summan(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        mehu = Tuote("Mehu", 5)
+        self.kori.lisaa_tuote(mehu)
+
+        self.assertEqual(self.kori.hinta(), 8)
 
 
         
