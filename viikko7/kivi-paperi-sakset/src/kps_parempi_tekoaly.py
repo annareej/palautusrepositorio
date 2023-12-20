4fb88d450796c2d240e17eb471b9411d0f3711f3
Anna-Reetta Julkunen <ar.julkunen@gmail.com>
2023-12-20 19:46:38 +0200

    viikko7 kps

---
+++ viikko7/kivi-paperi-sakset/src/kps_parempi_tekoaly.py
@@ -0,0 +1,13 @@
+from tekoaly_parannettu import TekoalyParannettu
+from kivi_paperi_sakset import KiviPaperiSakset
+
+
+class KPSParempiTekoaly(KiviPaperiSakset):
+    def __init__(self):
+        self.tekoaly = TekoalyParannettu(10)
+
+    def _toisen_siirto(self, ensimmaisen_siirto):
+        toisen_siirto = self.tekoaly.anna_siirto()
+        print(f"Tietokone valitsi: {toisen_siirto}")
+        self.tekoaly.aseta_siirto(ensimmaisen_siirto)
+        return toisen_siirto
